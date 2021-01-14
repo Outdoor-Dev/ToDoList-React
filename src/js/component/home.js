d7c8b46356028d7613e7beecbe13b3a2a677b042
Outdoor-Dev <saltairpix@gmail.com>
2021-01-14 11:32:23 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState([
+		"Walk dog",
+		"Wash clothes",
+		"Clean house"
+	]);
+	const [userInput, setUserInput] = useState("");
+
+	const handleKeyUp = () => {
+		if (event.keyCode === 13) {
+		} else {
+			
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="todo-title"> Todos</h1>
+			<div className="todo-list">
+				<input
+					className="tasks"
+					type="text"
+					onChange={e => setUserInput(e.target.value)}
+					value={userInput}
+					onKeyUp={handleKeyUp}
+				/>
+
+				<ul className="list-group">
+					{task.map((value, index) => {
+						return (
+							<li className="list-group-item" key={index}>
+								{value}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 }
